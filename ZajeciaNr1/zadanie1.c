513ce338aa07036c539497342ccd391cd3d2fc82
Łukasz Kocurek <146734977+Koci45@users.noreply.github.com>
2024-02-27 12:48:31 +0100

    Create zadanie1.c

---
+++ ZajeciaNr1/zadanie1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    char input[24];
+    printf("Enter a string: ");
+
+    scanf("%s", input); 
+    
+    printf("You entered: %s\n", input);
+}
